5ca225d241b7c73cb6fa7c087fb61ac42366eb56
mi-skam <maksim.codes@mailbox.org>
2022-12-28 21:31:43 +0100

    typos

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,18 @@
 module.exports = {
+  /**
+   * We need to explicitly enable all prettier plugins, as to ensure
+   * prettier-plugin-tailwindcss get loaded the last.
+   */
   plugins: [
     'prettier-plugin-css-order',
-    'prettier-plugin-import-sort',
     'prettier-plugin-jsdoc',
     'prettier-plugin-organize-attributes',
     'prettier-plugin-organize-imports',
     'prettier-plugin-style-order',
-    'prettier-plugin-tailwindcss', // must be last
+    'prettier-plugin-tailwindcss',
   ],
   pluginSearchDirs: false,
+
   tailwindConfig: './tailwind.config.js',
   singleQuote: true,
 };
